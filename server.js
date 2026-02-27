5c7d072bd7f0943edf91996a285bd6b5cfee0f72
Dinesh Kumar <dineshkumar.s@yaantran.com>
2026-02-27 11:46:13 +0530

    ssl

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: process.env.DB_HOST.includes("amazonaws.com")
+        ? { rejectUnauthorized: false }
+        : false
 });
 
 // Test database connection
